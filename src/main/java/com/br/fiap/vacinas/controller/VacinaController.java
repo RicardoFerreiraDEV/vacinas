27cf112be4bcf1dea4bbf46ff67f2820fa6958e4
Vacinas-Backend <ricardo.ferreira.fiap@gmail.com>
2021-12-25 21:33:27 -0300

    Commit Initial

---
+++ src/main/java/com/br/fiap/vacinas/controller/VacinaController.java
@@ -0,0 +1,29 @@
+package com.br.fiap.vacinas.controller;
+
+import com.br.fiap.vacinas.model.Vacinas;
+import com.br.fiap.vacinas.repository.VacinaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class VacinaController {
+
+    @Autowired
+    private VacinaRepository vacinaRepository;
+
+    @RequestMapping("/vacinas")
+    public List<Vacinas> vacinas(){
+        return vacinaRepository.findAll();
+    }
+
+    @PostMapping
+    public void save(@RequestBody Vacinas vacinas) {
+        vacinaRepository.save(vacinas);
+    }
+
+}
